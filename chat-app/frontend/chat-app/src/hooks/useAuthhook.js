e13cafc09e17400975bf6dec1d4394cb940c13cb
dhimanshruti <158125501+dhimanshruti@users.noreply.github.com>
2026-05-12 18:17:17 +0530

    Fix questions cors

--- chat-app/frontend/chat-app/src/hooks/useAuthhook.js
+++ chat-app/frontend/chat-app/src/hooks/useAuthhook.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import axiosInstance from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
+let socket = null;
 
 const useAuthhook = create((set, get) => ({
   authUser: null,
