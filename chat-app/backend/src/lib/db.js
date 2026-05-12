9f22a9d5b0e4a32fa8b190b2724016ce688236e8
dhimanshruti <158125501+dhimanshruti@users.noreply.github.com>
2026-05-12 16:38:16 +0530

    Fix MongoDB connection

--- chat-app/backend/src/lib/db.js
+++ chat-app/backend/src/lib/db.js
@@ -1,14 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const db = async () =>{
-    
-try {
-    
-    const connect = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB connected ${connect.connection.host}`)      
-    
-} catch (error) {
-        console.log('MongoDB Connection Failed',error)
-    }
-    
-}
+export const db = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB Connection Failed", error);
+  }
+};
